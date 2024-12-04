1cefabb9a3c329cda264cc8ca1f97e9263a6508b
vina786 <vinay76b@gmail.com>
2024-12-04 14:07:07 +0530

    Add files via upload

---
+++ greater3.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class greater3 {
+    
+    public static void main(String args[])
+    {
+        int a=12;
+        int b=56;
+        int c=90;
+        if((a>b)&&(a>c))
+        {
+            System.out.println("a is greater");
+        }
+        else if((b>a)&&(b>c))
+        {
+            System.out.println("b is greater");
+        }
+        else
+        {
+            System.out.println("c is greater");
+
+        }
+            }
+}
