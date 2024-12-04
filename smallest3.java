93649bd3c73b3a083e639ecf815d730413a98095
vina786 <vinay76b@gmail.com>
2024-12-04 14:17:16 +0530

    Add files via upload

---
+++ smallest3.java
@@ -0,0 +1,24 @@
+ import java.util.*;
+public class smallest3 {
+    
+    public static void main(String args[])
+    {
+        int a=12;
+        int b=56;
+        int c=90;
+        if((a<b)&&(a<c))
+        {
+            System.out.println("a is smallest");
+        }
+        else if((b<a)&&(b<c))
+        {
+            System.out.println("b is smaller");
+        }
+        else
+        {
+            System.out.println("c is smaller");
+
+        }
+    
+}
+}
