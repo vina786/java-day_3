362f1d102d0e452c8cc6ffd5804c736e11cd32a0
vina786 <vinay76b@gmail.com>
2024-12-04 13:53:12 +0530

    Add files via upload

---
+++ multipleof4.java
@@ -0,0 +1,14 @@
+public class multipleof4 {
+    public static void main(String args[])
+    {
+        int a=64;
+        if((a&3)==0)
+        {
+            System.out.println("multiple of 4");
+        }
+        else{
+            System.out.println("not a multiple");
+
+        }
+    }
+}
