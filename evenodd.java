ba3ad64ed7cd583fe688fd3b75a63c978b180b15
vina786 <vinay76b@gmail.com>
2024-12-04 10:58:47 +0530

    Add files via upload

---
+++ evenodd.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class evenodd {
+    public static void main(String args[]){
+int a=6;
+if((a&1)==0){
+    System.out.println("even");
+}
+else
+{
+    System.out.println("odd");
+}
+
+}
+    
+}
