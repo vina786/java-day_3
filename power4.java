e61adaf600f5e724bba398689b7b23fad64e975f
vina786 <vinay76b@gmail.com>
2024-12-04 13:36:45 +0530

    Add files via upload

---
+++ power4.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class power4 {
+    public static void main(String args[])
+    {
+        int a=64336;
+        if((a&(a-2))==0)
+        System.out.println("power of 4");
+        else
+        System.out.println(" not a power 4");
+
+    }
+    
+}
