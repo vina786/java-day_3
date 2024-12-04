d6b6e929f6d7a0eaaec487ac0a0a4899eddfd7a4
vina786 <vinay76b@gmail.com>
2024-12-04 10:59:16 +0530

    Add files via upload

---
+++ setbit.java
@@ -0,0 +1,16 @@
+public class setbit {
+    
+        public static void main(String args[])
+    {
+int a=10;
+int pos=1;
+if((a&(1<<pos))!=0){
+    System.out.println("set bit");
+}
+else{
+    System.out.println("not a setbit");
+
+}
+
+    }
+}
